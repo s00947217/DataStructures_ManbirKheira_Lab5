ec68bed798626a590e27fa0d91b429f9b14eb456
s00947217 <149434428+s00947217@users.noreply.github.com>
2024-11-17 20:45:02 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+class Main {
+  public static void main(String[] args) {
+
+    // Step 1 - Create a BST tree object called lab5Tree
+
+    // Step 2 - Insert the following values: 13, 22, 36, 5, 48, 17, 39, 2, 26, 40, 29, 34, 10
+
+    // Step 3 - Delete the value 17
+
+    // Step 4 - Traverse and output the values using inorder (sorted)
+
+    // Step 5 - Traverse and output the values using postorder
+
+    // Step 6 - Traverse and output the values using preorder
+
+    // Step 7 - Display the result of a search for the value 36
+
+    // Step 8 - Display the result of a search for the value 37
+
+    // Step 9 - Using the path() method, display the path from the root to 2
+
+    // Step 10 - Display the path from the root to 34
+
+  }
+}
